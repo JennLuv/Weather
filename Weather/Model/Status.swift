f46ee6e187e9aa43fd36f551d728e335f72ae6aa
Jennifer <57284701+JennLuv@users.noreply.github.com>
2024-07-15 13:37:25 +0700

    separate status enum from weather model

---
+++ Weather/Model/Status.swift
@@ -0,0 +1,18 @@
+//
+//  Status.swift
+//  Weather
+//
+//  Created by Jennifer Luvindi on 15/07/24.
+//
+
+import SwiftUI
+
+struct Status: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Status()
+}
