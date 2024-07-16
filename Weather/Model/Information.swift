45a46a782944ceb9dc2ce6a1a89ff44b916cab91
HalizaSyafa27 <oktavianihaliza@gmail.com>
2024-07-16 10:32:42 +0700

    push UVIModalityView, bikin struct app image, bikin information, edit AQIModalityView, edit safetyStatusCardComponentView, edit SafetyStatusDetailComponentView

---
+++ Weather/Model/Information.swift
@@ -0,0 +1,8 @@
+//
+//  Information.swift
+//  Weather
+//
+//  Created by Haliza Syafa Oktaviani on 16/07/24.
+//
+
+import Foundation
---
+++ Weather/Scenes/PlaceWeatherDetail/Components/Main Components with Preview/UVIModalityView.swift
@@ -0,0 +1,18 @@
+//
+//  UVIModalityView.swift
+//  Weather
+//
+//  Created by Haliza Syafa Oktaviani on 16/07/24.
+//
+
+import SwiftUI
+
+struct UVIModalityView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UVIModalityView()
+}
